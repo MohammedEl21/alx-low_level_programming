79055acb13805a54c961493950aa7062f0359a44
MohammedEl21 <106510022+MohammedEl21@users.noreply.github.com>
2022-09-16 01:17:32 -0400

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,35 +10,30 @@
 
 int main(void)
 {
-	int num = 1;
+	int x;
 
-	while (num++ < 100)
+	for (x = 1; x <= 100; x++)
 	{
-		if ((num % 3 == 0) && (num % 5 == 0))
+		if (x % 3 == 0 || x % 5 == 0)
 		{
-			printf("FizzBuzz ");
-		}
-		else if ((num % 3) == 0)
-		{
-			printf("Fizz ");
-		}
-		else if ((num % 5) == 0)
-		{
-			if (num != 100)
+			if (x % 3 == 0)
 			{
-				printf("Buzz ");
+				printf("Fizz");
 			}
-			else
+			if (x % 5 == 0)
 			{
 				printf("Buzz");
 			}
 		}
 		else
 		{
-			printf("%d ", num);
+			printf("%d", x);
+		}
+		if (x != 100)
+		{
+		putchar(' ');
 		}
 	}
-	printf("\n");
-
+	putchar('\n');
 	return (0);
 }
